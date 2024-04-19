75c82915e057b1653ff342f61fb0ca3ee4c05e43
Noordicsea <gtnoordsy24@icloud.com>
2024-04-19 14:21:40 -0500

    Added in password functionality.

--- src/main/java/org/biggertin/blockchat/Blockchat.java
+++ src/main/java/org/biggertin/blockchat/Blockchat.java
@@ -1,12 +1,19 @@
 package org.biggertin.blockchat;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public final class Blockchat extends JavaPlugin implements Listener {
+
+    private final Set<Player> authenticatedPlayers = new HashSet<>();
+    private final String password = "password";
 
     @Override
     public void onEnable() {
@@ -16,12 +23,19 @@ public final class Blockchat extends JavaPlugin implements Listener {
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
     }
 
     @EventHandler
     public void onPlayerChat(AsyncPlayerChatEvent event) {
-        event.getPlayer().sendMessage("You cannot chat!");
-        event.setCancelled(true);
+        Player player = event.getPlayer();
+        if (!authenticatedPlayers.contains(player)) {
+            if (event.getMessage().equalsIgnoreCase(password)) {
+                authenticatedPlayers.add(player);
+                player.sendMessage("You have been authenticated! You can now chat freely.");
+            } else {
+                player.sendMessage("You cannot chat until you provide the correct password! Please type the correct password!");
+                event.setCancelled(true);
+            }
+        }
     }
 }
