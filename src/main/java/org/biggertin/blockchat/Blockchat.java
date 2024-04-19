56d075d4cee7427d40b1189b188b9501c06c34ea
Noordicsea <gtnoordsy24@icloud.com>
2024-04-19 14:40:41 -0500

    Added ability to remember cleared players.

--- src/main/java/org/biggertin/blockchat/Blockchat.java
+++ src/main/java/org/biggertin/blockchat/Blockchat.java
@@ -8,30 +8,63 @@ import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 public final class Blockchat extends JavaPlugin implements Listener {
 
     private final Set<Player> authenticatedPlayers = new HashSet<>();
-    private final String password = "password";
+    private String password; // Changed to non-final since it will be loaded from config
 
     @Override
     public void onEnable() {
         Bukkit.getPluginManager().registerEvents(this, this);
-        getLogger().info("!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! Blockchat enabled !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+        saveDefaultConfig(); // Save the default configuration
+        password = getConfig().getString("password", "password"); // Load password from config
+        getLogger().info("Blockchat enabled!");
+        // Load authenticated players from config
+        List<String> canChatUsernames = getConfig().getStringList("canChat");
+        for (String username : canChatUsernames) {
+            Player player = Bukkit.getPlayerExact(username);
+            if (player != null) {
+                authenticatedPlayers.add(player);
+            }
+        }
     }
 
     @Override
     public void onDisable() {
+        // Save the "can chat" list to config on disable
+        List<String> canChatUsernames = getConfig().getStringList("canChat");
+        for (Player player : authenticatedPlayers) {
+            if (!canChatUsernames.contains(player.getName())) {
+                canChatUsernames.add(player.getName());
+            }
+        }
+        getConfig().set("canChat", canChatUsernames);
+        saveConfig();
     }
 
     @EventHandler
     public void onPlayerChat(AsyncPlayerChatEvent event) {
         Player player = event.getPlayer();
         if (!authenticatedPlayers.contains(player)) {
-            if (event.getMessage().equalsIgnoreCase(password)) {
+            String message = event.getMessage();
+            if (message.equalsIgnoreCase(password) || message.toLowerCase().startsWith(password.toLowerCase() + " ")) {
                 authenticatedPlayers.add(player);
                 player.sendMessage("You have been authenticated! You can now chat freely.");
+                // Add player to "can chat" list in config
+                List<String> canChatUsernames = getConfig().getStringList("canChat");
+                if (!canChatUsernames.contains(player.getName())) {
+                    canChatUsernames.add(player.getName());
+                    getConfig().set("canChat", canChatUsernames);
+                    saveConfig();
+                }
+                if (message.length() > password.length() + 1) {
+                    // Send everything after the password as a chat message.
+                    Bukkit.getServer().broadcastMessage("<" + player.getDisplayName() + "> " + message.substring(password.length() + 1));
+                }
+                event.setCancelled(true); // Cancel the event to prevent the original message from being sent.
             } else {
                 player.sendMessage("You cannot chat until you provide the correct password! Please type the correct password!");
                 event.setCancelled(true);
